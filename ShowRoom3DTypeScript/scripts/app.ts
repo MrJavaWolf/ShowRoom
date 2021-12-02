14fd0ce928d6debc58c5407bf6b2f2be762af37d
Jens Larsen <mr.javawolf@gmail.com>
2021-12-02 01:26:29 +0100

    Improved: Can scale the 3d model

--- ShowRoom3DTypeScript/scripts/app.ts
+++ ShowRoom3DTypeScript/scripts/app.ts
@@ -11,17 +11,43 @@ function createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     bounceLight.intensity = 0.5;
 
     // Camera
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0), scene);
+    const camera = AddCamera(scene, canvas);
+
+    // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
+    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+    ground.setEnabled(true);
+
+
+    BABYLON.SceneLoader.Append("models/", "Pants_sculpt.obj", scene, function (scene) {
+        // do something with the scene
+        var pants = scene.getMeshByName("pasted__pasted__pasted__Body_mesh1");
+        pants.scaling.x = 0.01;
+        pants.scaling.y = 0.01;
+        pants.scaling.z = 0.01;
+        pants.rotation.x = 0;
+        pants.rotation.y = Math.PI;
+        pants.rotation.z = 0;
+        console.log("Found pants: " + pants);
+    });
+
+
+    return scene;
+}
+
+function AddCamera(scene: BABYLON.Scene, canvas: HTMLCanvasElement) {
+    let pointTarget = new BABYLON.Vector3(0, 1, 0)
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 7, pointTarget, scene);
     camera.attachControl(canvas, true);
     // Target the camera to scene origin
-    camera.lockedTarget = BABYLON.Vector3.Zero();
+    camera.lockedTarget = pointTarget;
     camera.allowUpsideDown = false;
-    camera.lowerRadiusLimit = 1;
-    camera.upperRadiusLimit = 25;
-
+    camera.lowerRadiusLimit = 2;
+    camera.upperRadiusLimit = 7;
+    camera.wheelPrecision = 65;
     // Attach the camera to the canvas
     camera.attachControl(canvas, false);
 
+    // Add camera light
     var cameraLight = new BABYLON.SpotLight('cameraLight1', new BABYLON.Vector3(0.5, 0.5, 0), new BABYLON.Vector3(0, 0, 0), 1, 40, scene);
     scene.onBeforeRenderObservable.add(() => {
 
@@ -34,32 +60,19 @@ function createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
         //console.log("global_position: " + global_position + ", camera position: " + camera.position);
     })
 
+    return camera;
+}
 
-
+function AddTorusKnot(scene: BABYLON.Scene) {
     // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
     //var box = BABYLON.Mesh.CreateSphere('box1', 10, 3, scene);
-    var box = BABYLON.Mesh.CreateTorusKnot('box1', 2, 0.5, 128, 128, 2, 3, scene);
+    var torusKnot = BABYLON.Mesh.CreateTorusKnot('torusKnot', 2, 0.5, 128, 128, 2, 3, scene);
     var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
     myMaterial.diffuseColor = new BABYLON.Color3(0, 0, 1);
     myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
-    box.material = myMaterial;
-    box.position.y = 1;
-
-    // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
-    ground.setEnabled(true);
-
-
-    BABYLON.SceneLoader.Append("models/", "Pants_sculpt.obj", scene, function (scene) {
-        // do something with the scene
-        var pants = scene.getMeshByName("Pants_sculpt");
-        console.log("Found pants: " + pants);
-        //pants.scaling = new BABYLON.Vector3(0.01, 0.01, 0.01);
-
-    });
-
-
-    return scene;
+    torusKnot.material = myMaterial;
+    torusKnot.position.y = 1;
+    return torusKnot;
 }
 
 function changeColor(scene: BABYLON.Scene, color: string) {
